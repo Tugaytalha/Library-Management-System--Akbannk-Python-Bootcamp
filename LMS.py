70f51523ae24df0f490411eec12841efcbff3993
Tugay Talha İçen <76882910+Tugaytalha@users.noreply.github.com>
2024-02-17 16:26:41 +0300

    Requiments pdf added

--- LMS.py
+++ LMS.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 # This is a simple library management system that allows users to list, add, remove, and search books, as well as add
-# and remove members. Users can also borrow and return books. The system uses text files to store book and member
+# members. Users can also borrow and return books. The system uses text files to store book and member
 # information, and borrowed books. The system is built using the tkinter library for the GUI.
 
 # LibraryGUI class is the main class that creates the GUI for the library management system. It has methods for each
